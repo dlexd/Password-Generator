a8b19955d3fdee71d14ba656ca1b3abaf81d931a
dlexd <117990925+dlexd@users.noreply.github.com>
2023-03-24 20:42:15 -0700

    Added functionality
    Added code to the script.js file to give functionality in the random password generator.

--- script.js
+++ script.js
@@ -1,5 +1,29 @@
-// Assignment code here
+// All possible characters for a random password
+var possibleChars = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!@#$%^&*()_+~`|}{[]\:;?><,./-=";
 
+// Function to generate a random password
+function generatePassword() {
+
+  // Asks the user how long they want their password to be
+  var passwordLength = prompt("Enter the length of your password (between 8 and 128 characters):");
+
+  // A check to see if the user entered a length between 8-128 characters
+  if (passwordLength < 8 || passwordLength > 128) {
+    alert("Enter a password length between 8-128 characters.");
+    return "";
+  }
+  var password = "";
+
+  // Generates password based on password length and randomizes characters
+  for (var i = 0; i < passwordLength; i++) {
+    var randomIndex = Math.floor(Math.random() * possibleChars.length);
+    var randomChar = possibleChars[randomIndex];
+    password += randomChar;
+  }
+
+  // Returns the generated password
+  return password;
+}
 
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
